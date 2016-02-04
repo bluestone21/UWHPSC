342227ab9ae6c2c839db7909af74da9caa7e732d
Mustafa M Somalya <mustafamsomalya@icloud.com>
2016-02-05 00:48:37 +0530

    Sqrt func updated

--- makesqrt.py
+++ makesqrt.py
@@ -1,5 +1,10 @@
-def makesqrt(x,k=100,s=1.0):
-    for i in range(k):
+def makesqrt(x=4,kmax=100,s=1.0,tol=1.0e-14):
+    for k in range(kmax):
+        s0 = s
         s = 0.5 * (s + x/s)
+        delta_s = s-s0
+        if abs(delta_s/x) < tol:
+            break
 
+    print "It took %s iterations" %(k)
     return s
